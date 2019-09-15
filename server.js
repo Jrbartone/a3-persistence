28b418f47b3bffd172deae4cf4fb83c9ac57acb8
Glitch (hello-express) <none>
2019-09-15 17:05:24 +0000

    🐉🚕 Checkpoint ./server.js:6267243/32

--- server.js
+++ server.js
@@ -105,6 +105,7 @@ const translateWord = function(word, lang){
 }
 
 app.use(express.static('public'));
+app.use(passport.initialize());
 //app.use(favicon("https://cdn.glitch.com/706656bd-050b-4068-a255-39e940af21ae%2F35-512.png?v=1568561418524"))
 
 // http://expressjs.com/en/starter/basic-routing.html
